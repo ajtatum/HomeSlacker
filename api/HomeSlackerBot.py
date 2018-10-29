235b4dfcc85cb2d74219bd9995663b5363af6675
AJ Tatum <andrew.j.tatum@gmail.com>
2018-10-28 20:01:13 -0400

    Updated to an api

---
+++ api/HomeSlackerBot.py
@@ -0,0 +1,38 @@
+import os
+import logging
+import time
+import json
+from slackclient import SlackClient
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+logging.basicConfig()
+
+AppID = config['Slack']['AppID']
+ClientID = config['Slack']['ClientID']
+ClientSecret = config['Slack']['ClientSecret']
+SigningSecret = config['Slack']['SigningSecret']
+VerificationToken = config['Slack']['VerificationToken']
+OAUTHAccessToken = config['Slack']['OAUTHAccessToken']
+BotUserOAuthToken = config['Slack']['BotUserOAuthToken']
+ChannelID = config['Slack']['TestChannelID']
+
+slack_client = SlackClient(BotUserOAuthToken)
+
+
+#if slack_client.rtm_connect(with_team_state=False):
+#    print("Successfully connected, listening for events")
+#    while True:
+#        print(slack_client.rtm_read())
+#         
+#        time.sleep(1)
+#else:
+#    print("Connection Failed")
+class HomeSlackerBot:
+    def PostMessage(self, message):
+        slack_client.api_call(
+            "chat.postMessage",
+            channel=ChannelID,
+            text=message
+        )
---
+++ api/api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request #import main Flask class and request object
+import json
+from HomeSlackerBot import HomeSlackerBot
+
+app = Flask(__name__) #create the Flask app
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+#@app.route('/messsage', methods=['GET'])
+#def message():
+
+@app.route('/notify')
+def query_example():
+    message = request.args.get('message') #if key doesn't exist, returns None
+
+    hsb = HomeSlackerBot()
+    HomeSlackerBot.PostMessage(hsb, message)
+
+    return '''<h1>The message value is: {}</h1>'''.format(message)
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.10', debug=True, port=5000) #run app in debug mode on port 5000
