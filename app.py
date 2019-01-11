2a5e6bded8cd8e6d528f29797ada8cd8acee832a
AJ Tatum <andrew.j.tatum@gmail.com>
2019-01-11 00:10:42 -0500

    Added post methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request #import main Flask class and request object
+from flask import Flask, request, Response #import main Flask class and request object
 import json
 from HomeSlackerBot import HomeSlackerBot
 
@@ -24,7 +24,7 @@ def notify():
 
     return 'HomeSlacker is on it!'
 
-@app.route('/read')
+@app.route('/read', methods=['POST'])
 def read():
     message = request.args.get('message') #if key doesn't exist, returns None
     channelID = request.args.get('channel', default=config['Slack']['TestChannelID'])
@@ -33,6 +33,16 @@ def read():
     HomeSlackerBot.ReadMessage(hsb, channelID, message)
 
     return 'HomeSlacker is on it!'
+
+@app.route('/slack', methods=['POST'])
+def inbound():
+    #if request.form.get('token') == SLACK_WEBHOOK_SECRET:
+    channel = request.form.get('channel_name')
+    username = request.form.get('user_name')
+    text = request.form.get('text')
+    inbound_message = username + " in " + channel + " says: " + text
+    print(inbound_message)
+    return Response(), 200
 
 if __name__ == '__main__':
     app.run() #run app in debug mode on port 5000
